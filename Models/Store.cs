94110d58dced50593a2fe6dd30284b2549df046f
Fallexs <tristanfeebeck@gmail.com>
2023-11-15 02:43:43 -0700

    .

--- Models/Store.cs
+++ Models/Store.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CKK.Logic.Models {
+﻿namespace CKK.Logic.Models {
     public class Store {
         private int _Id;
         private string? _name;
