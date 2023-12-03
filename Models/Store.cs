5bf1cdd98c1b1174d06f61184734e789b4e7f466
Fallexs <tristanfeebeck@gmail.com>
2023-12-03 08:41:23 -0700

    Namespace Fix Possibly?

--- Models/Store.cs
+++ Models/Store.cs
@@ -1,65 +1,63 @@
-﻿namespace CKK.Logic {
-    namespace Models {
-        public class Store {
-            private int _id;
-            private string? _name;
-            private readonly List<StoreItem> _items = new();
+﻿namespace CKK.Logic.Models {
+    public class Store {
+        private int _id;
+        private string? _name;
+        private readonly List<StoreItem> _items = new();
 
-            public int GetId() => _id;
-            public string GetName() => _name ?? "Null";
+        public int GetId() => _id;
+        public string GetName() => _name ?? "Null";
 
-            public void SetId(int id) => _id = id;
-            public void SetName(string name) => _name = name;
+        public void SetId(int id) => _id = id;
+        public void SetName(string name) => _name = name;
 
-            public StoreItem? AddStoreItem(Product prod, int quantity) {
-                StoreItem _ = new(prod, quantity);
-                var SameStoreItem =
-                    from e in _items
-                    where prod == e.GetProduct()
-                    select e;
+        public StoreItem? AddStoreItem(Product prod, int quantity) {
+            StoreItem _ = new(prod, quantity);
+            var SameStoreItem =
+                from e in _items
+                where prod == e.GetProduct()
+                select e;
 
-                if( !SameStoreItem.Any() ) {
-                    _items.Add(_);
-                    return _;
-                }
+            if( !SameStoreItem.Any() ) {
+                _items.Add(_);
+                return _;
+            }
+            foreach( var item in SameStoreItem ) {
+                item.SetQuantity(quantity += item.GetQuantity());
+            }
+            return null;
+        }
+
+        public StoreItem? RemoveStoreItem(int id, int quantity) {
+            var SameStoreItem =
+                from e in _items
+                where id == e.GetProduct().GetId()
+                select e;
+            if( SameStoreItem.Any() ) {
                 foreach( var item in SameStoreItem ) {
-                    item.SetQuantity(quantity += item.GetQuantity());
-                }
-                return null;
-            }
-
-            public StoreItem? RemoveStoreItem(int id, int quantity) {
-                var SameStoreItem =
-                    from e in _items
-                    where id == e.GetProduct().GetId()
-                    select e;
-                if( SameStoreItem.Any() ) {
-                    foreach( var item in SameStoreItem ) {
-                        item.SetQuantity(item.GetQuantity() - quantity);
-                        if( item.GetQuantity() < 0 ) {
-                            item.SetQuantity(0);
-                        }
+                    item.SetQuantity(item.GetQuantity() - quantity);
+                    if( item.GetQuantity() < 0 ) {
+                        item.SetQuantity(0);
                     }
                 }
-                return null;
             }
+            return null;
+        }
 
-            public List<StoreItem> GetStoreItems() {
-                return _items;
-            }
+        public List<StoreItem> GetStoreItems() {
+            return _items;
+        }
 
-            public StoreItem? FindStoreItemById(int id) {
-                var FindByID =
-                    from e in _items
-                    where id == e.GetProduct().GetId()
-                    select e;
-                if( FindByID.Any() ) {
-                    foreach( var item in FindByID ) {
-                        return item;
-                    }
+        public StoreItem? FindStoreItemById(int id) {
+            var FindByID =
+                from e in _items
+                where id == e.GetProduct().GetId()
+                select e;
+            if( FindByID.Any() ) {
+                foreach( var item in FindByID ) {
+                    return item;
                 }
-                return null;
             }
+            return null;
         }
     }
 }
