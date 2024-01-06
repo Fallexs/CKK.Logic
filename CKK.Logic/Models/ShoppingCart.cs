23bdaeff2f67360a6efc1bad0b8fbdf1db131351
Fallexs <tristanfeebeck@gmail.com>
2024-01-06 07:59:46 -0700

    #

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -48,14 +48,13 @@ namespace CKK.Logic.Models
                 throw new ProductDoesNotExistException();
             } else {
                 foreach (var product in Products ) {
-                    var getQuantity = product.Quantity;
-                    getQuantity -= quant;
-                    if ( product.Quantity is < 0 ) {
+                    product.Quantity -= quant;
+                    if ( product.Quantity == 0 ) {
                         Products.Remove(product);
-                        product.Quantity = 0;
                         return product;
+                    } else if ( product.Quantity < 0 ) {
+                        Products.Remove(product);
                     } else {
-                        product.Quantity = getQuantity;
                         return product;
                     }
                 }
