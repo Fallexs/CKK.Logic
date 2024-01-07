77c0a4ca04a9372cc94d00bcd50cb21d6f40da4f
Fallexs <tristanfeebeck@gmail.com>
2024-01-06 19:35:11 -0700

    #

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -6,68 +6,84 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace CKK.Logic.Models
 {
-    public class Store : Entity, IStore {
-        public Store() {
-            items = new List<StoreItem>();
+    public class ShoppingCart : IShoppingCart {
+
+
+        public Customer Customer { get; set; }
+        public List<ShoppingCartItem> Products { get; set; }
+
+        public ShoppingCart(Customer cust) {
+            Customer = cust;
+            Products = new List<ShoppingCartItem>();
         }
-
-        public List<StoreItem> items { get; set; }
-        public StoreItem AddStoreItem(Product prod, int quantity) {
-            if( quantity <= 0 ) {
-                throw new InventoryItemStockTooLowException();
-
-            }
-
-            //checks if stock is empty
-            if( items.Count == 0 ) {
-                items.Add(new StoreItem(prod, quantity));
-                return items [ 0 ];
-            }
-            //checks if product is already in store
-            for( int i = 0; i < items.Count; i++ ) {
-                if( items [ i ].Product.Id == prod.Id ) {
-                    items [ i ].Quantity = (items [ i ].Quantity + quantity);
-                    return items [ i ];
-                }
-            }
-            //adds product if not present in current stock
-            items.Add(new StoreItem(prod, quantity));
-            return items.Last();
-
-
+        public int GetCustomerId() {
+            return Customer.Id;
         }
-        public StoreItem RemoveStoreItem(int id, int quantity) {
-            if( quantity < 0 ) {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            for( int i = 0; i < items.Count; i++ ) {
-                if( items [ i ].Product.Id == id ) {
-                    if( items [ i ].Quantity - quantity <= 0 ) {
-                        items [ i ].Quantity = (0);
-                        return items [ i ];
-                    }
-                    items [ i ].Quantity = (items [ i ].Quantity - quantity);
-                    return items [ i ];
-                }
-            }
-            throw new ProductDoesNotExistException();
-        }
-        public List<StoreItem> GetStoreItems() {
-            return items;
-        }
-        public StoreItem FindStoreItemById(int id) {
+        //gpbid not returning null when expected to
+        public ShoppingCartItem GetProductById(int id) {
             if( id < 0 ) {
                 throw new InvalidIdException();
+                return null;
             }
-            List<int> ids = items.Select(x => x.Product.Id).ToList();
-            for( int i = 0; i < items.Count; i++ ) {
-                if( ids [ i ] == id ) {
+            List<int> SampleData = Products.Select(x => x.Product.Id).ToList();
+            for( int i = 0; i < SampleData.Count; i++ ) {
 
-                    return items [ i ];
+                if( SampleData [ i ] == id ) {
+                    return Products [ i ];
                 }
             }
             return null;
+        }
+        public ShoppingCartItem AddProduct(Product prod, int quantity) {
+            //checks validity of argument
+            if( quantity <= 0 ) {
+                throw new InventoryItemStockTooLowException();
+            }
+
+            //checks if stock is empty
+            if( Products.Count == 0 ) {
+                Products.Add(new ShoppingCartItem(prod, quantity));
+                return Products [ 0 ];
+            }
+            //checks if product is already in store
+            for( int i = 0; i < Products.Count; i++ ) {
+                if( Products [ i ].Product.Id == prod.Id ) {
+                    Products [ i ].Quantity = (Products [ i ].Quantity + quantity);
+                    return Products [ i ];
+                }
+
+            }
+            //adds product if not present in current stock
+            Products.Add(new ShoppingCartItem(prod, quantity));
+            return Products.Last();
+        }
+        public ShoppingCartItem RemoveProduct(int id, int quantity) {
+            if( quantity < 0 ) {
+                throw new ArgumentOutOfRangeException();
+            }
+            for( int i = 0; i < Products.Count; i++ ) {
+                if( Products [ i ].Product.Id == id ) {
+
+
+                    if( Products [ i ].Quantity - quantity <= 0 ) {
+                        Products.Remove(Products [ i ]);
+                        return new ShoppingCartItem(null, 0);
+                    }
+                    Products [ i ].Quantity = (Products [ i ].Quantity - quantity);
+                    return Products [ i ];
+                }
+            }
+
+            throw new ProductDoesNotExistException();
+
+
+        }
+        public decimal GetTotal() {
+            decimal total = Products.Sum(x => x.GetTotal());
+            return total;
+        }
+        public List<ShoppingCartItem> GetProducts() {
+            return Products;
         }
 
     }
